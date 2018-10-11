58c15dd05a348c1edc83f7412bc4731b58e16053
RapeikaD <RapeikaD@gmail.com>
2018-10-11 14:50:41 +0300

    Post review
    Separate projects

--- Part 3/Task4/spec.js
+++ Part 3/Task4/spec.js
@@ -1,22 +1,8 @@
-// spec.js
-/*describe('Protractor Demo App', function() {
-  it('should add one and two', function() {
-    browser.get('http://juliemr.github.io/protractor-demo/');
-    element(by.model('first')).sendKeys(1);
-    element(by.model('second')).sendKeys(2);
-
-    element(by.id('gobutton')).click();
-
-    expect(element(by.binding('latest')).getText()).
-        toEqual('5'); // This is wrong!
-  });
-});*/
 
 describe('This is first ptr test which', function() {
 	it("should send email and verify Sent email", function() {
 
 		browser.waitForAngularEnabled(false);
-		//browser.get('/non-angular-login-page.html');
 
 		browser.get('http://gmail.com/');
 		element(by.name('identifier')).sendKeys('Retestd1@gmail.com');
@@ -27,7 +13,6 @@ describe('This is first ptr test which', function() {
 		browser.wait(element(by.css(".T-I")).isPresent())
   		browser.sleep(5000)
   		browser.get("https://mail.google.com/mail/#inbox?compose=new")
-		//element(by.xpath('//*[@id=":46"]/div/div')).click();
 		browser.sleep(5000)
 
 		var number = Math.round((Math.random()*100))
@@ -38,8 +23,6 @@ describe('This is first ptr test which', function() {
 		element(by.className('Am Al editable LW-avf')).sendKeys('You are the best!')
 		
 		browser.actions().keyDown(protractor.Key.CONTROL).sendKeys('\uE007').perform();
-		//browser.sleep(5000)
-		//window.open('https://mail.google.com/mail/#sent')
 		browser.get("https://mail.google.com/mail/#sent")
 		browser.sleep(5000)
 		
@@ -54,21 +37,13 @@ describe('This is first ptr test which', function() {
 				break;
 			}
 		}
-		
-		//let headerList = element.all(by.css('.bog'))
-		//let list = element.all(by.css('.y2'))
-		//expect(list.get(1).getText()).toBe(' - You are the best!')
-		//expect(headerList.get(1).getText()).toBe(RandomTitle)
-		
+	
 		browser.close()
 
 	})
 
 	it('should check presence of email sent from account 1', function() {
 			
-		//var browser2 = browser.forkNewDriverInstance();
-		//browser2.waitForAngularEnabled(false);		
-		//browser2.get('http://gmail.com/');
 		browser.restart()
 		browser.waitForAngularEnabled(false);
 		browser.get('http://gmail.com/')
